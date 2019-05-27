fd35153cce5207ea0d41ddc81529c315ebbc2261
magese <magese@live.cn>
2019-05-27 10:23:32 +0800

    Update lucene to 8.1.0

--- src/main/java/org/wltea/analyzer/cfg/Configuration.java
+++ src/main/java/org/wltea/analyzer/cfg/Configuration.java
@@ -1,6 +1,6 @@
 /*
- * IK 中文分词  版本 8.0.0
- * IK Analyzer release 8.0.0
+ * IK 中文分词  版本 8.1.0
+ * IK Analyzer release 8.1.0
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -21,8 +21,8 @@
  * 版权声明 2012，乌龙茶工作室
  * provided by Linliangyi and copyright 2012 by Oolong studio
  *
- * 8.0.0版本 由 Magese (magese@live.cn) 更新
- * release 8.0.0 update by Magese(magese@live.cn)
+ * 8.1.0版本 由 Magese (magese@live.cn) 更新
+ * release 8.1.0 update by Magese(magese@live.cn)
  *
  */
 package org.wltea.analyzer.cfg;
