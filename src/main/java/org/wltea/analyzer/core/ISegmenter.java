5ab517079bc42068212a989653cac64aa6087fe1
Magese <magese@live.cn>
2021-12-31 17:36:34 +0800

    注释格式化；

--- src/main/java/org/wltea/analyzer/core/ISegmenter.java
+++ src/main/java/org/wltea/analyzer/core/ISegmenter.java
@@ -29,21 +29,21 @@ package org.wltea.analyzer.core;
 
 
 /**
- *
  * 子分词器接口
  */
 interface ISegmenter {
 
-	/**
-	 * 从分析器读取下一个可能分解的词元对象
-	 * @param context 分词算法上下文
-	 */
-	void analyze(AnalyzeContext context);
+    /**
+     * 从分析器读取下一个可能分解的词元对象
+     *
+     * @param context 分词算法上下文
+     */
+    void analyze(AnalyzeContext context);
 
 
-	/**
-	 * 重置子分析器状态
-	 */
-	void reset();
+    /**
+     * 重置子分析器状态
+     */
+    void reset();
 
 }
